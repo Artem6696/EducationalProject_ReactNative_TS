0a64fa59d9c59ca99091eb0f04ce4d15e345f19c
Артем <hanya@MacBook-Pro.local>
2024-03-05 10:08:01 +0500

    load fonts in the root layout

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,40 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import {
-  ActivityIndicator,
-  FlatList,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
-import { useFonts, Inter_900Black } from "@expo-google-fonts/inter";
-import {
-  AmaticSC_400Regular,
-  AmaticSC_700Bold,
-} from "@expo-google-fonts/amatic-sc";
-import * as SplashScreen from "expo-splash-screen";
-import PartListItem from "../components/core/PartListItem";
-import { useEffect } from "react";
+import { FlatList, StyleSheet, View } from "react-native";
 
-SplashScreen.preventAutoHideAsync();
+import PartListItem from "../components/core/PartListItem";
+
 
 const data = [...Array(20)].map((value, ind) => ind + 1);
 
 export default function App() {
-  let [fonstLoaded, fontsError] = useFonts({
-    Inter: Inter_900Black,
-    AmaticRegular: AmaticSC_400Regular,
-    AmaticBold: AmaticSC_700Bold,
-  });
 
-  useEffect(() => {
-    if (fonstLoaded || fontsError) {
-      SplashScreen.hideAsync();
-    }
-  }, [fontsError, fonstLoaded]);
-
-  if (!fonstLoaded && !fontsError) {
-    return null;
-  }
 
   return (
     <View style={styles.container}>
--- src/app/platform/application1/index.tsx
+++ src/app/platform/application1/index.tsx
@@ -5,7 +5,7 @@ export default function Application1DetailsScreen() {
   return (
     <View>
        <Stack.Screen options={{ title: "part 1" }} />
-      <Text>Application_1DetailsScreen</Text>
+      <Text style={{fontFamily: 'AmaticBold', fontSize: 100}}>Application_1DetailsScreen</Text>
     </View>
   );
 }
