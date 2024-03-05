84dc46f7629a6795f869f39b33cbe5ff6257c5c8
Артем <hanya@MacBook-Pro.local>
2024-03-05 09:45:47 +0500

    stack navigator

--- src/app/platform/application1/index.tsx
+++ src/app/platform/application1/index.tsx
@@ -1,9 +1,10 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-
+import { Stack } from "expo-router";
 export default function Application1DetailsScreen() {
   return (
     <View>
+       <Stack.Screen options={{ title: "part 1" }} />
       <Text>Application_1DetailsScreen</Text>
     </View>
   );
